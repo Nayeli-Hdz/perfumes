cd16a25628b53a5a46d78b68f9e5e695ad499287
Nayeli Hdz <al221810668@gmail.com>
2021-04-11 16:41:00 -0500

    Creación del modelo Estados

---
+++ app/Models/estados.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class estados extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'idestados';
+    protected $fillable = ['idestados','nombre'];
+}
