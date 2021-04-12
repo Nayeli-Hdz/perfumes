fda5adb1260a68d7dd04ca1874bd16c2a0e5dc2c
Albert-Mon <82283603+Albert-Mon@users.noreply.github.com>
2021-04-11 23:19:27 -0500

    Nueva modificacion del codigo web2

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,21 @@ Route::post('Mensaje', [UsuariosController::class, 'Mensaje'])->name ('Mensaje')
 Route::post('guardarusuario', [UsuariosController::class, 'guardarusuario'])->name ('guardarusuario');
 
 Route::get('reporteusuarios', [UsuariosController::class, 'reporteusuarios'])->name ('reporteusuarios');
+
+//este recibe un parametro que es la id del empleado que se va a desactivar
+Route::get('desactivausuario/{id_usuario}',[UsuariosController::class, 'desactivausuario'])
+->name('desactivausuario');
+
+//Este recibe un parametro que es la id del empleado para que se active el empleado
+Route::get('activarusuario/{id_usuario}',[UsuariosController::class, 'activarusuario'])
+->name('activarusuario');
+
+//Este recibe un parametro que es la id del empleado para que se borre el empleado
+Route::get('borrausuario/{id_usuario}',[UsuariosController::class, 'borrausuario'])
+->name('borrausuario');
+
+//Este recibe un parametro que es la id del empleado para que se pueda modificar el empleado
+Route::get('modificausuario/{id_usuario}',[UsuariosController::class, 'modificausuario'])
+->name('modificausuario');
+
+Route::post('guardacambios', [UsuariosController::class, 'guardacambios'])->name ('guardacambios');
